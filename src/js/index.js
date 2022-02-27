f08a6b386f8bf1478a2e0addf27df7b3ea6b82c8
Otgonbayar <N1ckn@me1983>
2022-02-27 12:40:43 +0800

    Search input action and view results. Add loading.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,42 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from './view/searchView';
 
-let search = new Search('pizza');
+/**
+ * Web app төлөв
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкласан жорууд
+ * - Захиалж байгаа жорын найрлагууд
+ */
 
-search.doSearch()
-    .then(r => console.log('Result: ' + r));
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. Шинээр хайлтын обьектийг үүсгэж өгнө.
+    state.search = new Search(query);
+
+    // 3. Хайлт хийхэд зориулж дэлгэцийн UI-г бэлдэнэ.
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
+
+    // 4. Хайлтыг гүйцэтгэнэ.
+    await state.search.doSearch();
+
+    // 5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    clearLoader();
+    if(state.search.result === undefined) alert('Хайлтаар илэрцгүй...');
+    else searchView.renderRecipes(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -13,7 +13,7 @@ export default class Search {
 
             return this.result;
         } catch (error) {
-            alert('error: ' + error);
+            console.log('Алдаа: ' + error);
         }
         
     }
